f4736658d9b991e86ae5b91fc2ca733222970b98
David Ortinau <dave@rendr.io>
2017-01-17 08:22:29 -0600

    initial commit

---
+++ RegistrationForm/Pages/RegistrationFormPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace RegistrationForm.Pages
+{
+	public partial class RegistrationFormPage : ContentPage
+	{
+		public RegistrationFormPage()
+		{
+			InitializeComponent();
+
+			// why not change the form based on country selection
+		}
+	}
+}
